5f5c97c82a8398ca7c704ad18b08d90396987fcb
pepe619 <56723953+pepe619-ela@users.noreply.github.com>
2023-07-15 16:42:04 +0530

    Update urbandict_generator.py

--- urbandict_generator.py
+++ urbandict_generator.py
@@ -2,6 +2,8 @@
 import streamlit as st
 import requests
 import pandas as pd
+from io import BytesIO
+import base64
 
 def get_definition(term):
     url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define"
@@ -19,6 +21,34 @@ def get_definition(term):
     data = response.json()
     return data
 
+If you want to sort the DataFrame by the 'thumbs_up' column, you can use the sort_values() function provided by pandas. Here's how you can do this:
+
+python
+
+df1 = df1.sort_values('thumbs_up', ascending=False)
+
+This will sort the DataFrame in descending order by the 'thumbs_up' column. If you want to sort in ascending order, you can set ascending=True.
+
+Here is how your full Streamlit app would look with this change:
+
+python
+
+import streamlit as st
+import requests
+import pandas as pd
+from io import BytesIO
+import base64
+
+def get_definition(term):
+    url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define"
+    headers = {
+        "X-RapidAPI-Key": "your_api_key",
+        "X-RapidAPI-Host": "mashape-community-urban-dictionary.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers, params={"term": term})
+    data = response.json()
+    return data
+
 def app():
     st.set_page_config(layout="wide")  # Use the full page instead of a narrow central column
     st.title("Urban Dictionary Definition Generator")
@@ -29,8 +59,16 @@ def app():
             if 'list' in data:
                 df = pd.DataFrame(data['list'])
                 df1 = df[['word','definition','example','thumbs_up','thumbs_down']]
-		df1 = df1.sort_values('thumbs_up', ascending=False)  # Sort the DataFrame by 'thumbs_up'
-                st.write(df1)
+                df1 = df1.sort_values('thumbs_up', ascending=False)  # Sort the DataFrame by 'thumbs_up'
+                st.dataframe(df1)  # Use st.dataframe instead of st.write
+
+                # Convert DataFrame to Excel and create a download button
+                towrite = BytesIO()
+                df1.to_excel(towrite, index=False, sheet_name='Sheet1')  # write to BytesIO buffer
+                towrite.seek(0)  # reset pointer
+                b64 = base64.b64encode(towrite.read()).decode()  # read buffer and convert to base64
+                href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="your_filename.xlsx">Download Data</a>'
+                st.markdown(href, unsafe_allow_html=True)
             else:
                 st.write("No definition found.")
         else:
