2c5716fb7d324339906f0ed61b8b303d8342ebe8
pepe619 <56723953+pepe619-ela@users.noreply.github.com>
2023-07-15 16:14:55 +0530

    Create urbandict_generator.py

---
+++ urbandict_generator.py
@@ -0,0 +1,37 @@
+
+import streamlit as st
+import requests
+import pandas as pd
+
+def get_definition(term):
+    url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define"
+
+    #term = input("Please enter a term: ")
+    #querystring = {"term": term}
+  
+    headers = {
+	  "X-RapidAPI-Key": "7fd977a6b5msh913fd194d1d0ec3p162a93jsn6293361adae4",
+	  "X-RapidAPI-Host": "mashape-community-urban-dictionary.p.rapidapi.com"
+    }
+
+    #response = requests.get(url, headers=headers, params=querystring)
+    response = requests.get(url, headers=headers, params={"term": term})
+    data = response.json()
+    return data
+
+def app():
+    st.title("Urban Dictionary Definition Generator")
+    term = st.text_input("Enter a word to search its definition")
+    if st.button('Search'):
+        if term:
+            data = get_definition(term)
+            if 'list' in data:
+                df = pd.DataFrame(data['list'])
+                st.write(df)
+            else:
+                st.write("No definition found.")
+        else:
+            st.write("Please enter a word.")
+
+if __name__ == "__main__":
+    app()
